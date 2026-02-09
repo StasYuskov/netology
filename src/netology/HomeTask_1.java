c50939497c1adcbb25f4f08f04862071308e8454
Stas Yuskov <yuskov.itrum@gmail.com>
2026-02-09 14:26:45 +0300

    start commit

---
+++ src/netology/HomeTask_1.java
@@ -0,0 +1,10 @@
+package netology;
+
+public class HomeTask_1 {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int remittance = 500000000;
+        int result = balance + remittance;
+        System.out.println(result);
+    }
+}
